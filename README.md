f6c2dede1b68103b36a5fc8859b0638327bff43b
defdruil <defdruil@gmail.com>
2018-06-05 02:42:43 +0200

    Setup initial complet du projet

--- README.md
+++ README.md
@@ -25,3 +25,9 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## En cas de problème avec Pixi JS
+
+Vérifier que la version de pixi js installée est la bonne `npm install pixi.js@5.0.0-alpha.2`
+
+Copier le fichier `pixi.js.d.ts` présent à la racine du projet, vers le `dossier node_modules/pixi.js/pixi.js.d.ts`
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainCanvasComponent } from './main-canvas/main-canvas.component';
+import { LeftPanelComponent } from './left-panel/left-panel.component';
+import { RightPanelComponent } from './right-panel/right-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainCanvasComponent,
+    LeftPanelComponent,
+    RightPanelComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
